bb34e8e2be241058053ede319654f1b940f076d5
Vishal-217 <velidivishal217@gmail.com>
2025-05-07 18:23:53 +0530

    upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const toggleButton = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
-// Toggle the 'active' class on click
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
